2d4aaa0592a575c4f21bdad8eb5c0f68a65cc4b5
unknown <houssemeddinesouissi10@gmail.com>
2019-12-10 20:29:26 +0100

    Location Performance Improved + Permissions Handled

---
+++ app/src/main/java/com/brains404/cameragps/Constants.java
@@ -0,0 +1,20 @@
+package com.brains404.cameragps;
+
+class Constants {
+
+    /** CAMERA IMAGE CAPTURE REQUEST */
+    final static  int REQUEST_IMAGE_CAPTURE = 1 ;
+
+    /** LOCATION REQUEST **/
+    final static int GPS_REQUEST = 10;
+
+
+    /** Ask for  permission FINE_LOCATION, COARSE_LOCATION **/
+    final static int LOCATION_PERMISSION_REQUEST=100 ;
+
+    /** Ask for permissions FINE_LOCATION, COARSE_LOCATION, CAMERA **/
+    final static  int PERMISSIONS_REQUEST = 1000;
+
+
+
+}
--- app/src/main/java/com/brains404/cameragps/MainActivity.java
+++ app/src/main/java/com/brains404/cameragps/MainActivity.java
@@ -1,32 +1,47 @@
 package com.brains404.cameragps;
 
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.Locale;
 import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
-    static final int REQUEST_IMAGE_CAPTURE = 1;
     ImageView showPicture;
     Button takePicture;
-    private FusedLocationProviderClient fusedLocationClient;
+    TextView showLocation;
 
+    boolean isGPS = false;
+    private double mLatitude;
+    private double mLongitude;
 
-    LocationManager mLocationManager;
-
+    FusedLocationProviderClient mFusedLocationClient;
+    private LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +49,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         takePicture = findViewById(R.id.btn_take_pic);
         showPicture = findViewById(R.id.iv_show_pic);
-        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
+        showLocation = findViewById(R.id.tv_current_location);
         // on Take Picture Click send request to camera
         takePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dispatchTakePictureIntent();
+                //PATH :  checkPermissions -> dispatchTakePictureIntent ->onActivityResult->getCurrentLocation
+                checkPermissions();
             }
 
         });
@@ -49,31 +63,127 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void checkPermissions() {
+        // check for permissions if not granted [FINE_LOCATION,COARSE_LOCATION,CAMERA]
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // request for permissions
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.CAMERA},
+                    Constants.PERMISSIONS_REQUEST);
+        } else {
+            dispatchTakePictureIntent();
+        }
+    }
+
+    // Send Intent to take a picture and wait for the result
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            startActivityForResult(takePictureIntent, Constants.REQUEST_IMAGE_CAPTURE);
         }
     }
 
+    // Result come, check if there is a data
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        // Callback result from image request code , RESULT_OK system defined code [-1]
+        if (requestCode == Constants.REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) Objects.requireNonNull(extras).get("data");
             showPicture.setImageBitmap(imageBitmap);
-            // Get the last known location
-            fusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-
-                            if (location != null) {
-                                Toast.makeText(getApplicationContext(), location.toString(), Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
+            getCurrentLocation();
+        }
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Constants.GPS_REQUEST) {
+                isGPS = true; // flag maintain before get location
+            }
         }
     }
 
+    public void getCurrentLocation() {
+        //get Gps Status
+        new GpsUtils(this).turnGPSOn(new GpsUtils.onGpsListener() {
+            @Override
+            public void gpsStatus(boolean isGPSEnable) {
+                // turn on GPS
+                isGPS = isGPSEnable;
+            }
+        });
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        // location Request with interval
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(20 * 1000);
+
+
+        // check for permissions if not granted [FINE_LOCATION,COARSE_LOCATION]
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // request for permissions
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    Constants.LOCATION_PERMISSION_REQUEST);
+
+        } else {
+            // keep location updated to ensure we get a location even if last position doesn't exist
+            locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    if (locationResult == null) {
+                        return;
+                    }
+                    for (Location location : locationResult.getLocations()) {
+                        if (location != null) {
+                            //get updated location
+                            mLatitude = location.getLatitude();
+                            mLongitude = location.getLongitude();
+                            showLocation.setText(String.format(Locale.US, "%s ;; %s", mLatitude, mLongitude));
+                            mFusedLocationClient.getLastLocation();
+                        }
+                        // stop location callbacks
+                        if (mFusedLocationClient != null) {
+                            mFusedLocationClient.removeLocationUpdates(locationCallback);
+                        }
+
+                    }
+                }
+            };
+            // already permission granted
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        mLatitude = location.getLatitude();
+                        mLongitude = location.getLongitude();
+                        showLocation.setText(String.format(Locale.US, "%s , %s", mLatitude, mLongitude));
+                    }
+                }
+            });
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == Constants.LOCATION_PERMISSION_REQUEST) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission Granted so we get the location
+                mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            mLatitude = location.getLatitude();
+                            mLongitude = location.getLongitude();
+                            showLocation.setText(String.format(Locale.US, "%s , %s", mLatitude, mLongitude));
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
